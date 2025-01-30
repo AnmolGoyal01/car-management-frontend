6530ea0c627e68dfdf1bff92b076dc9bd9aeb55c
anmolgoyal1212 <anmolgoyal_ee21b6_59@dtu.ac.in>
2025-01-30 19:45:28 +0530

    add search functionality

--- src/pages/AllCarPage.tsx
+++ src/pages/AllCarPage.tsx
@@ -9,8 +9,9 @@ import { useNavigate } from "react-router-dom";
 const AllCarPage = () => {
   const navigate = useNavigate();
   const carData = useSelector((state: RootState) => state.cars);
-  const [cars, setCars] = useState<ICar[]>([]);
   const dispatch = useDispatch();
+  const [cars, setCars] = useState<ICar[]>([]);
+  const [searchQuery, setSearchQuery] = useState("");
 
   const loadCars = async () => {
     await CarActions.getAllCars({})(dispatch);
@@ -18,6 +19,7 @@ const AllCarPage = () => {
 
   useEffect(() => {
     loadCars();
+    setCars(carData.allCars.cars);
   }, []);
   useEffect(() => {
     setCars(carData.allCars.cars);
@@ -27,19 +29,47 @@ const AllCarPage = () => {
     if (e.target.tagName === "BUTTON") return;
     navigate(`/car/${id}`);
   };
+
+  const filterCars = async () => {
+    await CarActions.getAllCars({ search: searchQuery })(dispatch);
+    setCars(carData.allCars.cars);
+  };
+
   return (
-    <div className="grid grid-cols-1 md:grid-cols-3 gap-6 p-6">
-      {cars?.map((car) => (
-        <div
-          className="hover:cursor-pointer"
-          key={car._id}
-          onClick={(e) => {
-            handleCarClick(e, car._id);
-          }}
-        >
-          <CarCard car={car} />
+    <div className="p-6">
+      <div className="flex items-center justify-center">
+        <div className="flex items-center justify-center w-full mx-2 text-center">
+          <input
+            type="text"
+            placeholder="Search by title, description, or tags..."
+            value={searchQuery}
+            onChange={(e) => setSearchQuery(e.target.value)}
+            className="w-full p-2 mb-6 border rounded"
+          />
         </div>
-      ))}
+          <button
+            className="px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700"
+            onClick={() => filterCars()}
+          >
+            Search
+          </button>
+      </div>
+
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+        {cars?.length > 0 ? (
+          cars.map((car) => (
+            <div
+              className="hover:cursor-pointer"
+              key={car._id}
+              onClick={(e) => handleCarClick(e, car._id)}
+            >
+              <CarCard car={car} />
+            </div>
+          ))
+        ) : (
+          <p className="text-center col-span-3">No cars found</p>
+        )}
+      </div>
     </div>
   );
 };
